8af0deaa6651cb17bceefe6a5f227eee6ff49b85
EnamiHaise <enamihaise@gmail.com>
2024-07-03 23:58:39 +0900

    Final revision

--- heads_or_tails.py
+++ heads_or_tails.py
@@ -21,7 +21,7 @@ for round in range(1, 4):
 print(f"Heads: {heads_count}, Tails: {tails_count}")
 
 if heads_count > tails_count:
-    print("You won!")
+    print(f"{name} won!")
 else:
-    print("You lost!")
+    print(f"{name} lost!")
 
